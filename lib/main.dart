839046fe37e78e8ed76170126f02f1d0685837b4
rel1nce <101862863+rel1nce@users.noreply.github.com>
2023-02-23 17:19:46 +0300

    remove ios support

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   const Text('Show Chart'),
-                Switch.adaptive(
+                Switch(
                   value: _showChart,
                   onChanged: (value) {
                     setState(() {
